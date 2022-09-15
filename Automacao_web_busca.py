691b107fb6c045c01ff5a2e11519e5de92ac99a1
Guilherme-Fernandes97 <guilherme_fernandes1997@hotmail.com>
2022-09-14 22:09:15 -0300

    Projeto finalizado

--- Automacao_web_busca.py
+++ Automacao_web_busca.py
@@ -60,6 +60,7 @@ def busca_google_shopping(nav, produto, termos_banidos, preco_minimo, preco_maxi
                 preco = resultado.find_element(By.CLASS_NAME, 'a8Pemb').text
                 preco = preco.replace("R$", "").replace(" ", "").replace(".", "").replace(",", ".")
                 preco = float(preco)
+
                 # verificando se o preco ta dentro do minimo e maximo
                 if preco_minimo <= preco <= preco_maximo:
                     elemento_link = resultado.find_element(By.CLASS_NAME, 'aULzUe')
@@ -119,12 +120,51 @@ def busca_buscape(nav, produto, termos_banidos, preco_minimo, preco_maximo):
             pass
     return lista_ofertas
 
+#construção da lista de tabelas
 
-produto = 'iphone 12 64 gb'
-termos_banidos = 'mini watch'
-preco_minimo = 3500
-preco_maximo = 4500
+tabela_ofertas = pd.DataFrame()
 
-lista_ofertas_buscape = busca_buscape(nav, produto, termos_banidos, preco_minimo, preco_maximo)
-print(lista_ofertas_buscape)
+for linha in tabela_produtos.index:
+    produto = tabela_produtos.loc[linha, "Nome"]
+    termos_banidos = tabela_produtos.loc[linha, "Termos banidos"]
+    preco_minimo = tabela_produtos.loc[linha, "Preço mínimo"]
+    preco_maximo = tabela_produtos.loc[linha, "Preço máximo"]
 
+    lista_ofertas_google_shopping = busca_google_shopping(nav, produto, termos_banidos, preco_minimo, preco_maximo)
+    if lista_ofertas_google_shopping:
+        tabela_google_shopping = pd.DataFrame(lista_ofertas_google_shopping, columns=['produto', 'preco', 'link'])
+        tabela_ofertas = tabela_ofertas.append(tabela_google_shopping)
+    else:
+        tabela_google_shopping = None
+
+    lista_ofertas_buscape = busca_buscape(nav, produto, termos_banidos, preco_minimo, preco_maximo)
+    if lista_ofertas_buscape:
+        tabela_buscape = pd.DataFrame(lista_ofertas_buscape, columns=['produto', 'preco', 'link'])
+        tabela_ofertas = tabela_ofertas.append(tabela_buscape)
+    else:
+        tabela_buscape = None
+
+#exportar a base de ofertas
+
+tabela_ofertas = tabela_ofertas.reset_index(drop=True)
+tabela_ofertas.to_excel("Ofertas.xlsx", index=False)
+
+#enviar por email
+import win32com.client as win32
+
+if len(tabela_ofertas.index) > 0:
+    outlook = win32.Dispatch('outlook.application')
+    mail = outlook.CreateItem(0)
+    mail.To = 'jckzti@gmail.com'
+    mail.Subject = 'Produto(s) Encontrado(s) na faixa de preço desejada'
+    mail.HTMLBody = f"""
+    <p>Prezados,</p>
+    <p>Encontramos alguns produtos em oferta dentro da faixa de preço desejada. Segue tabela com detalhes</p>
+    {tabela_ofertas.to_html(index=False)}
+    <p>Qualquer dúvida estou à disposição</p>
+    <p>Att., Guilherme</p>
+    """
+
+    mail.Send()
+
+nav.quit()
